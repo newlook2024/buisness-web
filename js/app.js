194ab7484f517325856114e641c1cf8c11b690fc
newlook2024 <abdulvahob2022@gmail.com>
2024-02-24 20:13:42 +0500

    added javascript function for open btn

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ let oylik = document.querySelector('#monthly')
 let card = document.querySelector('.prices_card_box')
 let card1 = document.querySelector('.box1')
 
+let open = document.querySelectorAll('.open_text')
+let openBtn = document.querySelectorAll('.open_btn')
+
 burgerBtn.addEventListener('click', function(){
     responsive.style.left = '0'
 })
@@ -32,3 +35,12 @@ yillik.addEventListener('click', function(){
     oylik.classList.add('monthly')
 })
 
+function openFun (btnOpen, textOpen, textClose, height){
+    btnOpen.addEventListener('click', function(){
+        textOpen.style.height = height
+        textClose.style.height = '0'
+    })
+}
+
+openFun(openBtn[0] , open[0] , open[1], "120px")
+openFun(openBtn[1] , open[1] , open[0], "150px")
