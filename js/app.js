7145ccf242d8731f39d7db3a70d63294895b1ad9
newlook2024 <abdulvahob2022@gmail.com>
2024-02-29 17:03:10 +0500

    upp buton added

--- js/app.js
+++ js/app.js
@@ -11,14 +11,14 @@ let card1 = document.querySelector('.box1')
 let open = document.querySelectorAll('.open_text')
 let openBtn = document.querySelectorAll('.open_btn')
 
-burgerBtn.addEventListener('click', function(){
+burgerBtn.addEventListener('click', function () {
     responsive.style.left = '0'
 })
-closeBtn.addEventListener('click', function(){
+closeBtn.addEventListener('click', function () {
     responsive.style.left = '-100%'
 })
 
-oylik.addEventListener('click', function(){
+oylik.addEventListener('click', function () {
     card.style.left = '-600%'
     card1.style.right = '0'
     yillik.classList.add('monthly')
@@ -26,7 +26,7 @@ oylik.addEventListener('click', function(){
     oylik.classList.remove('monthly')
     oylik.classList.add('yearly')
 })
-yillik.addEventListener('click', function(){
+yillik.addEventListener('click', function () {
     card.style.left = '0%'
     card1.style.right = '-600%'
     yillik.classList.add('yearly')
@@ -35,12 +35,27 @@ yillik.addEventListener('click', function(){
     oylik.classList.add('monthly')
 })
 
-function openFun (btnOpen, textOpen, textClose, height){
-    btnOpen.addEventListener('click', function(){
+function openFun(btnOpen, textOpen, textClose, height) {
+    btnOpen.addEventListener('click', function () {
         textOpen.style.height = height
         textClose.style.height = '0'
     })
 }
 
-openFun(openBtn[0] , open[0] , open[1], "120px")
-openFun(openBtn[1] , open[1] , open[0], "150px")
+openFun(openBtn[0], open[0], open[1], "120px")
+openFun(openBtn[1], open[1], open[0], "150px")
+
+let dumaloq = document.querySelector('#upArrow')
+
+window.addEventListener('scroll', function () {
+    let a = Math.floor(this.window.pageYOffset)
+    console.log(a);
+    if(a > 1000){
+        dumaloq.style.right = '10px'
+    }else{
+        dumaloq.style.right = '-100%'
+    }
+    dumaloq.addEventListener('click', function(){
+        
+    })
+})
